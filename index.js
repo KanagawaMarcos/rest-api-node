e1bd4e0dfcd3f8eeb16a081359b942ce75518f5a
Marcos Costa <mcs1901@outlook.com>
2018-07-20 17:31:28 -0300

    Minor bug fix and using json flag in header

--- index.js
+++ index.js
@@ -33,10 +33,11 @@ const server =  http.createServer(function(req,res){
   req.on('data', function(data){
     buffer += decoder.write(data);
   });
+
   req.on('end', function(){
     buffer += decoder.end();
 
-    // Choose th ehandler this request should go to. If  not found, use 404
+    // Choose the handler this request should go to. If  not found, use 404
     let chosenHandler = handlers.notFound;
     if (typeof(router[trimmedPath]) !== 'undefined'){
        chosenHandler = router[trimmedPath];
@@ -50,21 +51,25 @@ const server =  http.createServer(function(req,res){
       'headers' : headers,
       'payload' : buffer
     };
-    // Routhe the request to the handler specified in the router
+    // Route the request to the handler specified in the router
     chosenHandler(data, function(statusCode, payload){
+
       // User the status code called back by the handler, or default 404
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      if (typeof(statusCode) !== 'number'){
+          statusCode = 200;
+      }
 
       // Use the payload called back by the handler, or default to empty object
-      payload = typeof(payload) == 'object' ? payload : {};
+      if(typeof(payload) !== 'object'){
+        payload = {};
+      }
 
       // Convert the payload to a string
       let payloadString = JSON.stringify(payload);
 
       // Return the response
+      res.setHeader('Content-Type', 'application/json');
       res.writeHead(statusCode);
-
-      // Send the response
       res.end(payloadString);
 
       // Log the request path
