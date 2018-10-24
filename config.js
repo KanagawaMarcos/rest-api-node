8798f71429d06960ae0e861d63774c800b36406c
Eduardo Roger Silva Nascimento <eduardo.roger@ufma.deinf.local>
2018-10-24 15:07:35 -0300

    add missing files

---
+++ config.js
@@ -0,0 +1,38 @@
+/*
+ * Create and export configuration variables
+ *
+ */
+
+// Container for all the environments
+let environments = {};
+
+// Staging (default) environment
+environments.staging = {
+  'http_port' : 3000,
+  'https_port' : 3001,
+  'envName' : 'staging',
+  'hashing_secret' : 'this_is_a_secret'
+};
+
+// Production environment
+environments.production = {
+  'http_port' : 5000,
+  'https_port' : 5001,
+  'env_name' : 'production',
+  'hashing_secret': 'this_is_also_a_secret'
+};
+
+// Determine which environment was passed as a command-line argument
+let current_enviroment = '';
+if (typeof(process.env.NODE_ENV) == 'string'){
+  current_enviroment = process.env.NODE_ENV.toLowerCase();
+}
+
+// Set the passed environment or the default
+let environment_to_export = environments.staging;
+if(typeof(environments[current_enviroment]) == 'object'){
+  environment_to_export = environments[current_enviroment];
+}
+
+// Export the module
+module.exports = environment_to_export;
--- index.js
+++ index.js
@@ -3,14 +3,117 @@
 *
 */
 
-//  Dependencies
+// Includes
 const http = require('http');
+const https = require('https');
+const url = require('url');
+const fs = require('fs');
+const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
+const _data = require('./lib/data');
+const handlers = require('./lib/handlers');
+const helpers = require('./lib/helpers');
 
-// Start the server, and have it listen on port 3000
-const server = http.createServer((request,response) =>{
-  response.end('Hello World\n');
+
+// Instantiate the HTTP server
+const http_server =  http.createServer((request,response) =>{
+  requestListener(request, response);
 });
 
-server.listen(3000, ()=>{
-  console.log('The server is listening on port 3000 now');
+// Start the HTTP server
+http_server.listen(config.http_port, () =>{
+  console.log("HTTP: Server listen to port ",config.http_port);
 });
+
+// HTTPS keys
+const https_server_options = {
+  'key' : fs.readFileSync('./https/key.pem'),
+  'cert' : fs.readFileSync('./https/cert.pem')
+};
+
+// Instantiate the HTTPS server
+const https_server =  https.createServer(https_server_options, (req,res) => {
+  requestListener(req, res,);
+});
+
+// Start the HTTPS server
+https_server.listen(config.https_port, ()=>{
+  console.log("HTTPS: Server listen to port ",config.https_port);
+});
+
+// All the server logic for both the http and https server
+const requestListener = (request, response) => {
+
+  // Get the URL and parse it
+  const parsedUrl = url.parse(request.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmed_path = path.replace(/^\/+|\/+$/g,'');
+
+  // Get the query string as an object
+  const query_string_object = parsedUrl.query;
+
+  // Get the HTTP Method
+  const method = request.method.toUpperCase();
+
+  // Get the headers as an object
+  const headers = request.headers;
+
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  request.on('data', (data)=>{
+    buffer += decoder.write(data);
+  });
+
+  request.on('end', ()=>{
+    buffer += decoder.end();
+
+    // Choose the handler this request should go to. If  not found, use 404
+    let chosenHandler = handlers.notFound;
+    if (typeof(router[trimmed_path]) !== 'undefined'){
+       chosenHandler = router[trimmed_path];
+    }
+
+    // Construct the data object to send to the handler
+    const data = {
+      'trimmed_path' : trimmed_path,
+      'query_string_object' : query_string_object,
+      'method' : method,
+      'headers' : headers,
+      'payload' : helpers.parseJsonToObject(buffer)
+    };
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, (status_code, payload) => {
+
+      // User the status code called back by the handler, or default 404
+      if (typeof(status_code) !== 'number'){
+          status_code = 200;
+      }
+
+      // Use the payload called back by the handler, or default to empty object
+      if(typeof(payload) !== 'object'){
+        payload = {};
+      }
+
+      // Convert the payload to a string
+      let payloadString = JSON.stringify(payload);
+
+      // Return the response
+      response.setHeader('Content-Type', 'application/json');
+      response.writeHead(status_code);
+      response.end(payloadString);
+
+      // Log the request path
+      console.log('Returning this response: ', status_code, payloadString);
+    });
+  });
+};
+
+// Define a request router
+const router = {
+  'ping': handlers.ping,
+  'users': handlers.users
+};
