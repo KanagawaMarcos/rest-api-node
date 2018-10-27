f7957881232aee356513d3348cfb85eae33d33a7
Marcos Costa <mcs1901@outlook.com>
2018-10-27 13:03:43 -0300

    change the default env to production

--- config.js
+++ config.js
@@ -10,15 +10,15 @@ let environments = {};
 
 // Staging (default) environment
 environments.staging = {
-  'http_port' : PORT,//'http_port' : 3000,
-  'https_port' : PORT,//'https_port' : 3001,
+  'http_port' : 3000,
+  'https_port' : 3001,
   'envName' : 'staging',
   'hashing_secret' : 'this_is_a_secret'
 };
 
 // Production environment
 environments.production = {
-  'http_port' : PORT,//'http_port' : 5000,
+  'http_port' : (PORT+1),//'http_port' : 5000,
   'https_port' : PORT, //'https_port' : 5001,
   'env_name' : 'production',
   'hashing_secret': 'this_is_also_a_secret'
@@ -31,7 +31,8 @@ if (typeof(process.env.NODE_ENV) == 'string'){
 }
 
 // Set the passed environment or the default
-let environment_to_export = environments.staging;
+//let environment_to_export = environments.staging;
+let environment_to_export = environments.production;
 if(typeof(environments[current_enviroment]) == 'object'){
   environment_to_export = environments[current_enviroment];
 }
